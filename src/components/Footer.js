58ebeb76cdbd32fae6df591b2684412e3820ab1e
PEACE IRABOR <poriaspec@gmail.com>
2022-04-14 09:38:47 +0100

    new updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ const  Footer =()=> {
 
       </ul>
 
-      <p> <span> <FontAwesomeIcon icon={faCopyright} className="fa" /> </span> 2022 Herocodes | All rights reserved</p>
+      <p> <span> <FontAwesomeIcon icon={faCopyright} className="fa" /> </span> 2022 herocodes | All rights reserved</p>
 
 
     </footer>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react'
 import "../css/Portfolio.css"
 import "aos/dist/aos.css"
 import Aos from 'aos'
-import portfolio1 from "../images/kash.png"
-import portfolio2 from "../images/mentorafrica.png"
-import portfolio3 from "../images/nirogstreet.png"
-import portfolio4 from "../images/find_am.png"
-import portfolio5 from "../images/travelTube.png"
-import portfolio6 from "../images/traveltube_part.png"
-import portfolio7 from "../images/T_courier.png"
+import portfolio1 from "../images/kashbase.jpg"
+import portfolio2 from "../images/mentorafrica.jpg"
+import portfolio3 from "../images/nirogstreet.jpg"
+import portfolio4 from "../images/findam.png"
+import portfolio5 from "../images/traveltube.jpg"
+import portfolio6 from "../images/traveltube_partner.png"
+import portfolio7 from "../images/Tcourier.jpg"
 
 
 const  Portfolio =()=> {
@@ -31,7 +31,7 @@ const  Portfolio =()=> {
         <div className='container portfolio_container'>
           <article className='portfolio_item'>
             <div className='portfolio_test-image'> <img src={portfolio1} alt="Kashbase" /> </div>
-            <h3>Kashbase: A digital finance app for savings and investment</h3>
+            <h3>Kashbase: A digital finance app for savings and investment.</h3>
           <span className='btnSpan'>
           <a href='https://play.google.com/store/apps/details?id=com.kashbase.spark&hl=en&gl=US' target="_blank" style={{ textDecoration: "none"}}  className='portBtn'>PlayStore</a>
           <a href='https://apps.apple.com/ng/app/kashbase-save-invest-now/id1498590590' target="_blank" style={{ textDecoration: "none"}} className='PortBtn2'>AppStore</a>
@@ -39,7 +39,7 @@ const  Portfolio =()=> {
           </article>
 
           <article className='portfolio_item'>
-            <div className='portfolio_test-image'><img src={portfolio2} alt="Mentor Africa" /> </div>
+            <div className='portfolio_test-image' ><img src={portfolio2} alt="Mentor Africa" /> </div>
             <h3> Mentor Africa: An app to connect mentors with mentees.</h3>
            <span className='btnSpan'>
            <a href='https://play.google.com/store/apps/details?id=com.sarge.maf_mentor.mentor&hl=en&gl=US' target="_blank"  style={{ textDecoration: "none"}} className='portBtn'>PlayStore</a>
@@ -50,7 +50,7 @@ const  Portfolio =()=> {
 
           <article className='portfolio_item'>
             <div className='portfolio_test-image'> <img src={portfolio3} alt="Nirog Street" /></div>
-            <h3> Nirog Street: An app for the Ayurvedic doctors and students</h3>
+            <h3> Nirog Street: An app for the Ayurvedic doctors and students.</h3>
            <span className='btnSpan'>
            <a href='https://play.google.com/store/apps/details?id=com.app.nirogstreet&hl=en&gl=US' target="_blank" style={{ textDecoration: "none"}} className='portBtn'>PlayStore</a>
             <a href='https://apps.apple.com/ng/app/nirogstreet/id1352321621' target="_blank"style={{ textDecoration: "none"}}  className='PortBtn2'>AppStore</a>
@@ -60,7 +60,7 @@ const  Portfolio =()=> {
           
           <article className='portfolio_item'>
             <div className='portfolio_test-image'> <img src={portfolio4} alt="Nirog Street" /></div>
-            <h3> Findam: A Food and Drinks Delivery App </h3>
+            <h3> Findam: An app for Food and Drinks Delivery.</h3>
            <span className='btnSpan'>
            <a href='https://play.google.com/store/apps/details?id=com.findamapp.find_am&hl=en&gl=US' target="_blank" style={{ textDecoration: "none"}} className='portBtn'>PlayStore</a>
             <a href='https://apps.apple.com/ng/app/findam/id1561266673' target="_blank" style={{ textDecoration: "none"}} className='PortBtn2'>AppStore</a>
@@ -69,7 +69,7 @@ const  Portfolio =()=> {
 
           <article className='portfolio_item'>
             <div className='portfolio_test-image'><img src={portfolio5} alt="Travel Tube" /></div>
-            <h3>TravelTube: An App that provides video content</h3>
+            <h3>TravelTube: An App that provides video content.</h3>
 
             <span className='btnSpan'>
 
@@ -82,7 +82,7 @@ const  Portfolio =()=> {
 
            <article className='portfolio_item'>
             <div className='portfolio_test-image'><img src={portfolio6} alt="TravelTube Partner Central" /></div>
-            <h3>TravelTube Partner Central: A food ordering App</h3>
+            <h3>TravelTube Partner Central: A food ordering and delivery App.</h3>
            <span className='btnSpan'>
            <a href='https://play.google.com/store/apps/details?id=com.traveltube.order_tracker' target="_blank" style={{ textDecoration: "none"}} className='portBtn'>PlayStore</a>
             <a href='https://apps.apple.com/ng/app/traveltube-partner-central/id1585107311' target="_blank" style={{ textDecoration: "none"}} className='PortBtn2'>AppStore</a>
